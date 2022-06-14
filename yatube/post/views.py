e1415875496b136fdb726ce9fa054c6427fadb84
Oleg Kiselyov <c0ncep2all@proton.me>
2022-06-14 22:29:14 +0300

    2themooon startpoint

---
+++ yatube/post/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Main page')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'about group {slug}')
